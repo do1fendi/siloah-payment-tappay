917eb04585e529422487905b5515d5cd11c6b944
Fendi <do1fendi@gmail.com>
2021-04-27 14:21:14 +0800

    added company footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="footer align-center">
-      <p><span>&#169;</span> Siloah Travel 2015 ~ {{new Date().getFullYear()}}</p> 
+  <div class="footer text-center mt-5">
+    
+        希羅亞旅行社股份有限公司 <br />
+        台灣台北市大安區復興南路一段137號14樓之3<br />
+        電話：02-27217300 <br />
+        傳真：02-27217179 <br />
+        信箱：service@siloahtravel.com <br>
+        <span>&#169;</span> Siloah Travel 2015 ~ {{ new Date().getFullYear() }}
+      
   </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style>
-.footer{
-    background-color: #434852;
-    padding: 20px;
-    text-align: center;
-    color: azure;
-    position: fixed;
-    width: 100%;
-    bottom: 0px;      
+.footer {
+  background-color: #434852;
+  padding: 20px;  
+  color: azure;
+  width: 100%;
 }
 </style>
